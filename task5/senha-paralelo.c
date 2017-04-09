51816fd593849f41d57664d0ba2f6175f0b32246
Gustavo Ciotto <gustavociotto@gmail.com>
2017-04-09 10:43:01 -0300

    simple version with commentaries

--- task5/senha-paralelo.c
+++ task5/senha-paralelo.c
@@ -1,3 +1,9 @@
+/* 
+* Task #5: A simple Pthread program to break a ZIP file.
+* Gustavo Ciotto Pinton RA117136
+* MO644 - Parallel Programming
+*/
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,6 +16,7 @@
 
 #define PASSWD_SIZE 500000
 
+/* Shared variables used among all threads. If found is true, all threads stop their computing. */
 unsigned int n_threads, *id, found = 0;
 char filename[100];
 const char finalcmd[300] = "unzip -P%d -t %s 2>&1";
@@ -17,6 +24,8 @@ pthread_t *break_zip_threads;
 
 FILE *popen(const char *command, const char *type);
 
+/* Thread function: iterates over a range of passwords. Sets found if it finds the password,
+   signaling other threads to stop computing. */
 void *break_zip_thread (void *rank) {
 
 	FILE * fp;
@@ -32,7 +41,6 @@ void *break_zip_thread (void *rank) {
 	for(i = i_start; i <= i_end && !found; i++){
 
 		sprintf((char*)&cmd, finalcmd, i, filename);
-		//printf("Comando a ser executado: %s \n", cmd); 
 
 		fp = popen(cmd, "r");	
 		while (!feof(fp)) {
@@ -49,6 +57,7 @@ void *break_zip_thread (void *rank) {
 
 }
 
+/* Creates, starts and waits all threads to finish */
 void break_zip_parallel () {
 
 	unsigned int i;
@@ -93,6 +102,7 @@ int main ()
 	id = (unsigned int *) malloc (n_threads * sizeof(unsigned int));
 
 	t_start = rtclock();
+	/* Calls parallel function */
 	break_zip_parallel();
 	t_end = rtclock();
 
