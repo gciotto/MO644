e07b837cd07b8ef218e83fcaad9067da26e3a982
Gustavo Ciotto <gustavociotto@gmail.com>
2017-06-24 21:14:03 -0300

    += removed from expressions

--- project/src/RingElement.cpp
+++ project/src/RingElement.cpp
@@ -21,18 +21,18 @@ RingElement::RingElement(std::string name, double length, int pass_method) {
 
 void Drift::pass(pos_t &e) {
 
-	e[0] += this->length * e[1];
-	e[2] += this->length * e[3];
+	e[0] = this->length * e[1] + e[0];
+	e[2] = this->length * e[3] + e[2];
 }
 
 void Quadrupole::pass(pos_t &e) {
 
-	e[1] += -e[0] / this->focal_distance;
-	e[3] += +e[2] / this->focal_distance;
+	e[1] = (-1.0 * e[0]) / this->focal_distance + e[1];
+	e[3] = e[2] / this->focal_distance + e[3];
 }
 
 void Sextupole::pass(pos_t &e) {
 
-	e[1] = (e[0]*e[0] - e[2]*e[2]) * (this->sextupole_strength * this->length) + e[1];
+	e[1] = (e[0]*e[0] + ( -1.0 * e[2]*e[2])) * (this->sextupole_strength * this->length) + e[1];
 	e[3] = (2.0 * e[0]*e[2]) * (this->sextupole_strength * this->length) + e[3];
 }
